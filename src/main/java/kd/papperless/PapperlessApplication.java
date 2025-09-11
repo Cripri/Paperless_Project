37736f67825038b661c7e106fb5e1f9f1e44c516
Cripre <farenhite97@naver.com>
2025-09-11 15:47:29 +0900

    first commit

---
+++ src/main/java/kd/papperless/PapperlessApplication.java
@@ -0,0 +1,13 @@
+package kd.papperless;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PapperlessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PapperlessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kd/papperless/PapperlessApplicationTests.java
@@ -0,0 +1,13 @@
+package kd.papperless;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PapperlessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
