7857841727e36e1beb9dd93978747ac215dd188c
Cripre <farenhite97@naver.com>
2025-09-26 18:27:31 +0900

    주말에수정하겟읍니다

--- src/main/java/kd/paperless/controller/get/MainController.java
+++ src/main/java/kd/paperless/controller/get/MainController.java
@@ -1,14 +1,28 @@
 package kd.paperless.controller.get;
 
+import kd.paperless.entity.Notice;
+import kd.paperless.repository.NoticeRepository;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+import org.springframework.data.domain.*;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+
+@RequiredArgsConstructor
 @Controller
 public class MainController {
 
-    @GetMapping(value = {"/index", "/", "home"})
-    public String home() {
-        return "/main/intro";
+    private final NoticeRepository noticeRepository;
+
+    @GetMapping(value = {"/","home"})
+    public String introNotices(Model model) {
+        List<Notice> items = noticeRepository.findPostedTop(PageRequest.of(0, 3));
+        model.addAttribute("items", items);
+        return "main/intro";
     }
 
     @GetMapping("/main/portal")
